36da3aac648f68b58b90597e97b1f95be1066ef8
vyctorvguimaraes <vyctor.guimaraes@conexia.com.br>
2022-05-30 09:13:07 -0300

    criando entidade anemica

---
+++ src/customer.ts
@@ -0,0 +1,15 @@
+class Customer {
+  id: string;
+
+  name: string;
+
+  address: string;
+
+  constructor(id: string, name: string, address: string) {
+    this.id = id;
+    this.name = name;
+    this.address = address;
+  }
+}
+
+export default Customer;
