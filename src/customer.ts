a3ed678f61726479d2826154f3b6ff17b3c4f532
vyctorvguimaraes <vyctor.guimaraes@conexia.com.br>
2022-05-30 09:53:03 -0300

    entidades

--- src/customer.ts
+++ src/customer.ts
@@ -5,10 +5,36 @@ class Customer {
 
   address: string;
 
+  active: boolean;
+
   constructor(id: string, name: string, address: string) {
     this.id = id;
     this.name = name;
     this.address = address;
+    this.active = false;
+    this.validate();
+  }
+
+  changeName(name: string): void {
+    this.name = name;
+    this.validate();
+  }
+
+  activate(): void {
+    this.active = true;
+  }
+
+  deactivate(): void {
+    this.active = false;
+  }
+
+  validate(): void {
+    if (!this.name || this.name.length === 0) {
+      throw new Error('Name is required');
+    }
+    if (!this.address || this.address.length === 0) {
+      throw new Error('Address is required');
+    }
   }
 }
 
