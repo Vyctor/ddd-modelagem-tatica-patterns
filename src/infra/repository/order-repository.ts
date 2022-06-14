659fa195804cded4af9229dfa412af5397198a00
Vyctor <vyctor.guimaraes@conexia.com.br>
2022-06-13 22:14:29 -0300

    implementando repository

--- src/infra/repository/order-repository.ts
+++ src/infra/repository/order-repository.ts
@@ -2,6 +2,7 @@ import OrderRepositoryInterface from '../../domain/repository/order-repository.i
 import Order from '../../domain/entity/order';
 import OrderModel from '../database/sequelize/model/order.model';
 import OrderItemModel from '../database/sequelize/model/order-item.model';
+import OrderItem from '../../domain/entity/order-item';
 
 class OrderRepository implements OrderRepositoryInterface {
   async create(entity: Order): Promise<void> {
@@ -25,19 +26,60 @@ class OrderRepository implements OrderRepositoryInterface {
   }
 
   async update(entity: Order): Promise<void> {
-    throw new Error('Method not implemented.');
+    await OrderModel.update(
+      {
+        id: entity.id,
+        customer_id: entity.customer_id,
+        total: entity.total,
+        items: entity.items.map((item) => ({
+          id: item.id,
+          name: item.name,
+          price: item.price,
+          product_id: item.product_id,
+          quantity: item.quantity,
+        })),
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+      },
+    );
   }
 
   async delete(id: string): Promise<void> {
-    throw new Error('Method not implemented.');
+    await OrderModel.destroy({
+      where: {
+        id,
+      },
+    });
   }
 
   async find(id: string): Promise<Order> {
-    throw new Error('Method not implemented.');
+    const order = await OrderModel.findOne({ where: { id } });
+
+    return new Order(
+      order.id,
+      order.customer_id,
+      order.items.map(
+        (orderItem) => new OrderItem(orderItem.id, orderItem.product_id, orderItem.name, orderItem.price, orderItem.quantity),
+      ),
+    );
   }
 
   async findAll(): Promise<Order[]> {
-    throw new Error('Method not implemented.');
+    const orders = await OrderModel.findAll();
+
+    return orders.map(
+      (order) =>
+        new Order(
+          order.id,
+          order.customer_id,
+          order.items.map(
+            (orderItem) => new OrderItem(orderItem.id, orderItem.product_id, orderItem.name, orderItem.price, orderItem.quantity),
+          ),
+        ),
+    );
   }
 }
 
